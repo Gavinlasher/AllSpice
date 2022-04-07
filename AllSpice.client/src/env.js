968b9011c774a10b11e2c37941cf253551977935
gavinlasher <Krgavin21@yahoo.com>
2022-04-07 16:54:01 -0600

    commit

--- AllSpice.client/src/env.js
+++ AllSpice.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:5001' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const useSockets = false
 export const domain = 'dev-d0zymnj2.us.auth0.com'
 export const clientId = 'jz7lvvBXnG4VNZVo8vW6OBdfkhyHIHlw'
